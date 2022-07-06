e2adbac80f6b375da0563f3d4e94c4a4bd7befd3
Josh <LakotaJosh@gmail.com>
2022-07-05 22:30:02 -0500

    Updated Minecraft Server Setup

--- minecraftServerInstall.sh
+++ minecraftServerInstall.sh
@@ -1,3 +1,9 @@
 #!/bin/ash
 
 echo "Installing Minecraft Server Components"
+echo "Installing openJDK17"
+apk add openjdk17-jre
+echo "Installing PaperMC Server Jar"
+
+## To-do: add logic to get latest build (cURL and JQ not installed by default. but cURL is needed at the very least to get version number)
+#curl 'https://api.papermc.io/v2/projects/paper/version_group/1.19/builds'
--- setup.sh
+++ setup.sh
@@ -14,3 +14,4 @@ read question
 if [$question == "1"]
 then
     ./minecraftServerInstall.sh
+fi
